72cb79a64063bf326eb2dee5b58ba919e35e89bf
anshudevil07 <himanshupalariya13@gmail.com>
2026-04-16 16:42:28 +0530

    Update 3D portfolio URL

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ import { FiPlay, FiEdit3, FiArrowRight, FiX, FiMaximize2, FiSun, FiMoon } from "
 import { useTheme } from "../hooks/useTheme";
 import "./HomePage.css";
 
-const DEMO_URL = "http://localhost:5173";
+const DEMO_URL = "https://3d-portfolio-b835b6nde-anshudevil07s-projects.vercel.app";
 
 const HomePage = () => {
   const navigate = useNavigate();
